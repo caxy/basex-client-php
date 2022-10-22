c545e06f212109bdf7c58df0200b43f11dbee847
Mathieu <github@kumy.net>
2022-10-22 13:55:39 +0200

    Updated code from upstream project

---
+++ src/BaseXException.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * PHP client for BaseX.
+ * Works with BaseX 7.0 and later
+ *
+ * Documentation: https://docs.basex.org/wiki/Clients
+ *
+ * (C) BaseX Team 2005-22, BSD License
+ */
+
+namespace Caxy\BaseX;
+
+class BaseXException extends \RuntimeException
+{}
